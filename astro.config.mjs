5d800c8554dc6201081c05b2f4729b4ecb2a5d7c
Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
2025-04-22 15:28:40 -0500

    Testing on Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,39 +3,20 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import { remarkReadingTime } from "./src/lib/readTime";
-
-import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel"; 
 
 export default defineConfig({
   output: "server",
   site: "https://www.airwartrail.com",
   integrations: [mdx(), react()],
-
   vite: {
     plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        "react-dom/server": "react-dom/server.edge",
-      },
-    },
-    ssr: {
-      noExternal: ['react-dom']
-    }
   },
-
   markdown: {
     remarkPlugins: [remarkReadingTime],
     drafts: true,
   },
-
-  adapter: cloudflare({
-    runtime: {
-      mode: 'local',
-      type: 'pages'
-    }
+  adapter: vercel({
+    imageService: true,
   }),
-
-  ssr: {
-    noExternal: ["@radix-ui/*"]
-  }
 });
