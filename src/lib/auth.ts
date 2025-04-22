b4049416f85518903d90803628f29979efa4321a
Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
2025-04-22 13:45:27 -0500

    Fix the env import

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,8 +12,7 @@ import { VerifyEmail } from "@/components/email/VerifyEmail";
 
 // Create a new Turso database connection
 const client = createClient({
-  url: "libsql://airwartrail-betterauth-cobaltwebtech.aws-us-east-1.turso.io", // Hardcode the URL
-  //url: import.meta.env.TURSO_DB_URL,
+  url: import.meta.env.TURSO_DB_URL,
   authToken: import.meta.env.TURSO_DB_TOKEN,
 });
 
