5627ada5a3b65589c53d385bec5710c842630455
Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
2025-04-24 10:34:35 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
   ],
   vite: {
     plugins: [tailwindcss()],
-    alias: {
+    alias: import.meta.env.PROD && {
       "react-dom/server": "react-dom/server.edge",
     },
   },
