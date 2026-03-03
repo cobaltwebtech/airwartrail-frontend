92babb78fd48148390bcc2e7343a69c64170151b
Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
2026-03-02 20:23:57 -0600

    Added Better Auth Monitoring

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,3 +1,4 @@
+import { sentinelClient } from "@better-auth/infra/client";
 import { stripeClient } from "@better-auth/stripe/client";
 import { adminClient, magicLinkClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
@@ -7,6 +8,7 @@ export const client = createAuthClient({
 	plugins: [
 		magicLinkClient(),
 		adminClient(),
+		sentinelClient(),
 		stripeClient({
 			subscription: true,
 		}),
